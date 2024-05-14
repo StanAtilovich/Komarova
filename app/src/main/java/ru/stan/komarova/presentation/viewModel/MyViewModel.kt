2a2234f8437654a3b91c602f2242331796f4d639
StanAtilovich <alano92@yandex.ru>
2024-05-14 17:13:08 +0300

    загружает 1 страница теперь фото загружать и сохрать во вью модели

--- app/src/main/java/ru/stan/komarova/presentation/viewModel/MyViewModel.kt
+++ app/src/main/java/ru/stan/komarova/presentation/viewModel/MyViewModel.kt
@@ -1,16 +1,9 @@
 package ru.stan.komarova.presentation.viewModel
 
 import android.text.InputFilter
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import ru.stan.komarova.domain.model.Offer
-import ru.stan.komarova.data.network.TicketsApi
 
 class MyViewModel : ViewModel() {
     var textChangedByUser = false
@@ -26,24 +19,6 @@ class MyViewModel : ViewModel() {
         MutableLiveData<String>()
     }
 
-
-    fun fetchOffersFromApi() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://run.mocky.io/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val roomApi = retrofit.create(TicketsApi::class.java)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val room = roomApi.getTicketsById()
-                offerLiveData.postValue(room.offers)
-            } catch (e: Exception) {
-                Log.e("MyViewModel", "Error fetching data: ${e.message}")
-            }
-        }
-    }
 
 
 
